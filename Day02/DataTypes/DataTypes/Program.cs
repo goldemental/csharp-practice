a20f120d11ea6eca8e79e74b8366f461c4e3280b
Dr.GoldementaL <90459748+goldemental@users.noreply.github.com>
2026-03-03 01:24:12 +0300

    Add files via upload

---
+++ Day02/DataTypes/DataTypes/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace DataTypes
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the first number:");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the second number:");
+            int number2 = int.Parse(Console.ReadLine());
+            int sum = number1 + number2;
+            
+            Console.WriteLine("The sum of {0} and {1} is {2}.", number1, number2, sum);
+        }
+    }
+}
