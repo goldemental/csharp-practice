95c0d70113ef8d8a9f38792aadc17ce72b59cd24
Dr.GoldementaL <90459748+goldemental@users.noreply.github.com>
2026-03-02 22:41:27 +0300

    Add files via upload

---
+++ Day01/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace HelloWorld
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Please enter your age:");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Your name is {0} and you are {1} years old.", name, age);
+        }
+    }
+}
