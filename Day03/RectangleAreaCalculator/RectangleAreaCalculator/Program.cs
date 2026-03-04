2ca52f2262eac089083a305c31d5984451f36735
Dr.GoldementaL <90459748+goldemental@users.noreply.github.com>
2026-03-04 17:25:15 +0300

    Add files via upload

---
+++ Day03/RectangleAreaCalculator/RectangleAreaCalculator/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Operators
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the length of the rectangle:");
+            int length = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the width of the rectangle:");
+            int width = int.Parse(Console.ReadLine());
+
+            int area = length * width;
+            Console.WriteLine("The area of the rectangle is {0}.", area);
+        }
+    }
+}
