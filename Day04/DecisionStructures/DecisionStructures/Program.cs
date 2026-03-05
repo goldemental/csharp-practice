a866daaf0b2f0de2186d9700056db7e39b59285c
Dr.GoldementaL <90459748+goldemental@users.noreply.github.com>
2026-03-05 20:36:57 +0300

    Add files via upload

---
+++ Day04/DecisionStructures/DecisionStructures/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace DecisionStructures
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number:");
+            int number = int.Parse(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("The number {0} is even.", number);
+            }
+            else
+            {
+                Console.WriteLine("The number {0} is odd.", number);
+            }
+        } 
+    }
+}
