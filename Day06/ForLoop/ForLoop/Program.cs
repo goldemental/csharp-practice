99f0e4805597c0b3b3e3e609ff8070637042d70c
Dr.GoldementaL <90459748+goldemental@users.noreply.github.com>
2026-03-07 02:26:12 +0300

    Add files via upload

---
+++ Day06/ForLoop/ForLoop/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ForLoop
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            for (int number = 1;  number <= 10; number++)
+                Console.WriteLine(number);
+        }
+    }
+}
