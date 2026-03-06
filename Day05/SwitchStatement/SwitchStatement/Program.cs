b9bf267196c4f8affd4ac30c159d67c2011b87da
Dr.GoldementaL <90459748+goldemental@users.noreply.github.com>
2026-03-06 23:29:04 +0300

    Add files via upload

---
+++ Day05/SwitchStatement/SwitchStatement/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace SwitchStatement
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a day number (1-7):");
+            int dayNumber = int.Parse(Console.ReadLine());
+
+            switch (dayNumber)
+            {
+                case 1:
+                    Console.WriteLine("Day {0} is Monday.", dayNumber);
+                    break;
+                case 2:
+                    Console.WriteLine("Day {0} is Tuesday.", dayNumber);
+                    break;
+                case 3:
+                    Console.WriteLine("Day {0} is Wednesday.", dayNumber);
+                    break;
+                case 4:
+                    Console.WriteLine("Day {0} is Thursday.", dayNumber);
+                    break;
+                case 5:
+                    Console.WriteLine("Day {0} is Friday.", dayNumber);
+                    break;
+                case 6:
+                    Console.WriteLine("Day {0} is Saturday.", dayNumber);
+                    break;
+                case 7:
+                    Console.WriteLine("Day {0} is Sunday.", dayNumber);
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid day number. Please enter a number between 1 and 7.");
+                    break;
+            }
+        }
+    }
+}
