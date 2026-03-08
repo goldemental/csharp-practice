02bb800ad6f0c0455d3fc3b0bcc60470cf5e10a1
Dr.GoldementaL <90459748+goldemental@users.noreply.github.com>
2026-03-08 20:22:40 +0300

    Add files via upload

---
+++ Day07/WhileLoop/WhileLoop/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace WhileLoop
+{
+    internal class Program
+        {
+            private static void Main(string[] args)
+            {
+                Console.WriteLine("please enter a number:");
+                int number = int.Parse(Console.ReadLine());
+                int counter = 1;
+                
+                while (counter <= number)
+                { 
+                        Console.WriteLine(counter);
+                        counter++; 
+                }
+            }
+        }
+}
