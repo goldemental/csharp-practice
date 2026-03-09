dd30fe25377c87fb62884f604aadb62a2dff7dda
Dr.GoldementaL <90459748+goldemental@users.noreply.github.com>
2026-03-09 23:11:32 +0300

    Add files via upload

---
+++ Day08/LoopsForeach/LoopsForeach/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LoopsForeach
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string[] names = { "Alice", "Bob", "Charlie", "David", "Emma" };
+
+            Console.WriteLine("Names in the list:");
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
