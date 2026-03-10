b37800dab732f0179b91708bd6476d52688d27f5
Dr.GoldementaL <90459748+goldemental@users.noreply.github.com>
2026-03-10 23:38:43 +0300

    Add files via upload

---
+++ Day09/Arrays/Arrays/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ArraysDay9
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] numbers = { 5, 10, 15, 20, 25 };
+
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum = sum + numbers[i];
+            }
+
+            Console.WriteLine("The sum of the numbers is {0}.", sum);
+        }
+    }
+}
