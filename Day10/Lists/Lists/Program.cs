4aaeddba1d688a738cfe017ce8d797d0df47f044
Dr.GoldementaL <90459748+goldemental@users.noreply.github.com>
2026-03-11 23:26:47 +0300

    Add files via upload

---
+++ Day10/Lists/Lists/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            List<string> fruits = new List<string>
+            {
+                "Apple",
+                "Banana",
+                "Peach",
+                "Dragon Fruit",
+                "Strawberry"
+            };
+
+            Console.WriteLine("My favorite fruits:");
+
+            foreach (string fruit in fruits)
+            {
+                Console.WriteLine(fruit);
+            }
+        }
+    }
+}
