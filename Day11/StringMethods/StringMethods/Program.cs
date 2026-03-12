9cc6b2f972b5c25dc25afa94cd8d3210ac627921
Dr.GoldementaL <90459748+goldemental@users.noreply.github.com>
2026-03-12 23:14:34 +0300

    Add files via upload

---
+++ Day11/StringMethods/StringMethods/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StringMethods
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your full name:");
+            string fullName = Console.ReadLine();
+
+            string upperName = fullName.ToUpper();
+
+            Console.WriteLine("Your name in uppercase is: {0}", upperName);
+        }
+    }
+}
