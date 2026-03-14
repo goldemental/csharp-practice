1f002665a721191edbe35f23e5a11e84f85bbd2a
Dr.GoldementaL <90459748+goldemental@users.noreply.github.com>
2026-03-14 22:12:23 +0300

    Add files via upload

---
+++ Day12/StringConcatenation/StringConcatenation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StringConcatenation
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first name:");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name:");
+            string lastName = Console.ReadLine();
+
+            string fullName = firstName + " " + lastName;
+
+            Console.WriteLine("Hello " + fullName + "! Welcome.");
+        }
+    }
+}
