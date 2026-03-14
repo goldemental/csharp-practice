287172d0fe2434382d4f4fb1cc7be0bd62774dbd
Dr.GoldementaL <90459748+goldemental@users.noreply.github.com>
2026-03-14 22:27:44 +0300

    Add files via upload

---
+++ Day13/MathClass/MathClass/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MathClass
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number:");
+            double number = double.Parse(Console.ReadLine());
+
+            double squareRoot = Math.Sqrt(number);
+
+            Console.WriteLine("The square root of {0} is {1}.", number, squareRoot);
+        }
+    }
+}
