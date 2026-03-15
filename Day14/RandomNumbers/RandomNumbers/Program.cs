1da893853de51ee086147011fa13e59560a0b8d8
Dr.GoldementaL <90459748+goldemental@users.noreply.github.com>
2026-03-15 23:27:42 +0300

    Add files via upload

---
+++ Day14/RandomNumbers/RandomNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RandomNumbers
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(1, 101);
+            Console.WriteLine("Guess the number (between 1 and 100):");
+            int guess = int.Parse(Console.ReadLine());
+            if (guess == randomNumber)
+            {
+                Console.WriteLine("Correct! You guessed the number.");
+            }
+            else
+            {
+                Console.WriteLine("Wrong guess. The number was {0}.", randomNumber);
+            }
+        }
+    }
+}
