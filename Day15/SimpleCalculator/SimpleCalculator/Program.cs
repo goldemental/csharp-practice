b28a650968fb45933cb7b069d906a2b99f015ad3
Dr.GoldementaL <90459748+goldemental@users.noreply.github.com>
2026-03-16 23:50:28 +0300

    Add files via upload

---
+++ Day15/SimpleCalculator/SimpleCalculator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number:");
+            int number1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter an operator (+, -, *, /):");
+            string op = Console.ReadLine();
+
+            Console.WriteLine("Enter the second number:");
+            int number2 = int.Parse(Console.ReadLine());
+
+            int result = 0;
+
+            if (op == "+")
+            {
+                result = number1 + number2;
+            }
+            else if (op == "-")
+            {
+                result = number1 - number2;
+            }
+            else if (op == "*")
+            {
+                result = number1 * number2;
+            }
+            else if (op == "/")
+            {
+                result = number1 / number2;
+            }
+
+            Console.WriteLine("Result: {0}", result);
+        }
+    }
+}
