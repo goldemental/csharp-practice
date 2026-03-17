8b1a27c4fab908ae53de407cce373dee76981577
Dr.GoldementaL <90459748+goldemental@users.noreply.github.com>
2026-03-17 21:17:46 +0300

    Add files via upload

---
+++ Day16/Methods/Methods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Methods
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int number = int.Parse(Console.ReadLine());
+
+            int result = Square(number);
+
+            Console.WriteLine("The square of {0} is {1}.", number, result);
+        }
+
+        static int Square(int number)
+        {
+            return number * number;
+        }
+    }
+}
