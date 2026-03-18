7e2973765f8ee90585a87b7a101f0b90574c4ff1
Dr.GoldementaL <90459748+goldemental@users.noreply.github.com>
2026-03-18 23:51:52 +0300

    Add files via upload

---
+++ Day17/ReturnValues/ReturnValues/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ReturnValues
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number:");
+            int number1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            int number2 = int.Parse(Console.ReadLine());
+
+            int largerNumber = GetLargerNumber(number1, number2);
+
+            Console.WriteLine("The larger number is {0}.", largerNumber);
+        }
+
+        static int GetLargerNumber(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+    }
+}
