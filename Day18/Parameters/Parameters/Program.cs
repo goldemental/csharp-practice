6431b384e65095752689d2a9cc5235d5b6dfa77a
Dr.GoldementaL <90459748+goldemental@users.noreply.github.com>
2026-03-19 22:49:26 +0300

    Add files via upload

---
+++ Day18/Parameters/Parameters/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Parameters
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+
+            GreetUser(name);
+        }
+
+        static void GreetUser(string userName)
+        {
+            Console.WriteLine("Hello, {0}! Welcome.", userName);
+        }
+    }
+}
