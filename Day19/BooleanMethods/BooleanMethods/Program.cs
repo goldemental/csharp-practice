78503214d7b1a4e4c2ac9762ff1df92ef127d414
Dr.GoldementaL <90459748+goldemental@users.noreply.github.com>
2026-03-20 22:43:48 +0300

    Add files via upload

---
+++ Day19/BooleanMethods/BooleanMethods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BooleanMethods
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int number = int.Parse(Console.ReadLine());
+
+            bool isPositive = IsPositive(number);
+
+            Console.WriteLine("Is the number positive? {0}", isPositive);
+        }
+
+        static bool IsPositive(int number)
+        {
+            if (number > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
