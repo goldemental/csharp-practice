678c66ddb65026435b9c38d8424fdef483f5e641
Dr.GoldementaL <90459748+goldemental@users.noreply.github.com>
2026-03-21 23:44:58 +0300

    Add files via upload

---
+++ Day20/TryCatch/TryCatch/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TryCatch
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a number:");
+                int number = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("You entered: {0}", number);
+            }
+            catch
+            {
+                Console.WriteLine("Invalid input! Please enter a valid number.");
+            }
+        }
+    }
+}
