e95974d96513bf949a6dafafa64f2a0e291fadd9
Dr.GoldementaL <90459748+goldemental@users.noreply.github.com>
2026-03-23 23:49:46 +0300

    Add files via upload

---
+++ Day22/TwoDArrays/TwoDArrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TwoDArrays
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[,] matrix =
+            {
+                {1, 2},
+                {3, 4}
+            };
+
+            Console.WriteLine("Matrix elements:");
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.WriteLine(matrix[i, j]);
+                }
+            }
+        }
+    }
+}
