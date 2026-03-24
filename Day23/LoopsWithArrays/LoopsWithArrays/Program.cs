dd39bc0fafffe03057f21c7b1605158518240ff0
Dr.GoldementaL <90459748+goldemental@users.noreply.github.com>
2026-03-24 22:32:28 +0300

    Add files via upload

---
+++ Day23/LoopsWithArrays/LoopsWithArrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LoopsWithArrays
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Enter number {0}:", i + 1);
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+
+            Console.WriteLine("The sum of the numbers is {0}.", sum);
+        }
+    }
+}
