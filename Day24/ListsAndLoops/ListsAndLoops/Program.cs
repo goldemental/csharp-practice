894af94fb5d42f102c080801b6a74f6c299f1316
Dr.GoldementaL <90459748+goldemental@users.noreply.github.com>
2026-03-25 22:04:59 +0300

    Add files via upload

---
+++ Day24/ListsAndLoops/ListsAndLoops/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListsAndLoopsDay24
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>
+            {
+                1,
+                4,
+                2,
+                6,
+                3
+            };
+
+            Console.WriteLine("Numbers greater than 3:");
+
+            foreach (int number in numbers)
+            {
+                if (number > 3)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+        }
+    }
+}
