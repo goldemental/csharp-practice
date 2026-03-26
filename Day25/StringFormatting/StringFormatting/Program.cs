931f6a8e589cb39c4b604ce9558478daad0e48f3
Dr.GoldementaL <90459748+goldemental@users.noreply.github.com>
2026-03-26 22:40:11 +0300

    Add files via upload

---
+++ Day25/StringFormatting/StringFormatting/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StringFormatting
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter your age:");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Your name is {name} and you are {age} years old.");
+        }
+    }
+}
