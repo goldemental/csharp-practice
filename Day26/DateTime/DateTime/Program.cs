eadef9525c762c4839bba38d7837113da1063c4c
Dr.GoldementaL <90459748+goldemental@users.noreply.github.com>
2026-03-28 01:33:19 +0300

    Add files via upload

---
+++ Day26/DateTime/DateTime/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DateTimeDay26
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            DateTime today = DateTime.Now;
+
+            Console.WriteLine($"Today's date: {today.ToShortDateString()}");
+            Console.WriteLine($"Day of the week: {today.DayOfWeek}");
+        }
+    }
+}
