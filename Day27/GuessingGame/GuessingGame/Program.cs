47211513557ba1879577f14215c77d0cb1e2553f
Dr.GoldementaL <90459748+goldemental@users.noreply.github.com>
2026-03-28 22:49:01 +0300

    Add files via upload

---
+++ Day27/GuessingGame/GuessingGame/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Random random = new Random();
+            int secretNumber = random.Next(1, 101);
+
+            int guess = 0;
+
+            while (guess != secretNumber)
+            {
+                Console.WriteLine("Guess the number (1-100):");
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess > secretNumber)
+                {
+                    Console.WriteLine("Too high!");
+                }
+                else if (guess < secretNumber)
+                {
+                    Console.WriteLine("Too low!");
+                }
+                else
+                {
+                    Console.WriteLine("Correct! You guessed the number.");
+                }
+            }
+        }
+    }
+}
