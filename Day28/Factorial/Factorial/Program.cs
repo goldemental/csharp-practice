3670e9243a29cc9e06ae1155984a0e675d294725
Dr.GoldementaL <90459748+goldemental@users.noreply.github.com>
2026-03-29 23:56:48 +0300

    Add files via upload

---
+++ Day28/Factorial/Factorial/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Factorial
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int number = int.Parse(Console.ReadLine());
+
+            int result = Factorial(number);
+
+            Console.WriteLine("Factorial of {0} is {1}", number, result);
+        }
+
+        static int Factorial(int n)
+        {
+            int result = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                result = result * i;
+            }
+
+            return result;
+        }
+    }
+}
