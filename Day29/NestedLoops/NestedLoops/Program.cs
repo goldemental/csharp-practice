4ca4e5f0e32083dff394cd20662f75029067b360
Dr.GoldementaL <90459748+goldemental@users.noreply.github.com>
2026-03-30 21:01:22 +0300

    Add files via upload

---
+++ Day29/NestedLoops/NestedLoops/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NestedLoops
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 5; j++)
+                {
+                    int result = i * j;
+                    Console.Write("{0}\t", result);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
